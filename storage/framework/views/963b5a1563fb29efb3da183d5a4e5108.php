1649bf0623cd1ded0d98e6f96419083cffc3915b
Tobi Kudayisi <32499735+tkud04@users.noreply.github.com>
2026-03-17 14:09:34 +0100

    updates to layout

--- storage/framework/views/963b5a1563fb29efb3da183d5a4e5108.php
+++ storage/framework/views/963b5a1563fb29efb3da183d5a4e5108.php
@@ -203,349 +203,11 @@ $bday = $isLoggedIn ? $user->bday : '';
                       </div>
                     </div>
                     <ul class="menu clearfix">
-                      <li>
-                        <a class="active" href="index.html">HOME</a>
-                        <ul>
-                          <li>
-                            <a href="../green/index.html">Home 1</a>
-                          </li>
-                          <li>
-                            <a href="../blue/index.html">Home 2</a>
-                          </li>
-                          <li>
-                            <a href="../orange/index.html">Home 3</a>
-                          </li>
-                          <li>
-                            <a href="../pink/index.html">Home 4</a>
-                          </li>
-                          <li>
-                            <a href="../brown/index.html">Home 5</a>
-                          </li>
-                          <li>
-                            <a href="../green2/index.html">Home 6</a>
-                          </li>
-                          <li>
-                            <a href="../blueorange/index.html">Home 7</a>
-                          </li>
-                          <li>
-                            <a href="../blueorange2/index.html">Home 8</a>
-                          </li>
-                          <li>
-                            <a href="../browngreen/index.html">Home 9</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li class="mega-menu-container">
-                        <a href="#">SHOP</a>
-                        <div class="mega-menu clearfix">
-                          <div class="col-5">
-                            <a href="category.html" class="mega-menu-title">Clothing</a>
-                            <ul class="mega-menu-list clearfix">
-                              <li>
-                                <a href="#">Dresses</a>
-                              </li>
-                              <li>
-                                <a href="#">Jeans &amp; Trousers</a>
-                              </li>
-                              <li>
-                                <a href="#">Blouses &amp; Shirts</a>
-                              </li>
-                              <li>
-                                <a href="#">Tops &amp; T-Shirts</a>
-                              </li>
-                              <li>
-                                <a href="#">Jackets &amp; Coats</a>
-                              </li>
-                              <li>
-                                <a href="#">Skirts</a>
-                              </li>
-                            </ul>
-                          </div>
-                          <div class="col-5">
-                            <a href="category.html" class="mega-menu-title">Shoes</a>
-                            <ul class="mega-menu-list clearfix">
-                              <li>
-                                <a href="#">Formal Shoes</a>
-                              </li>
-                              <li>
-                                <a href="#">Casual Shoes</a>
-                              </li>
-                              <li>
-                                <a href="#">Sandals</a>
-                              </li>
-                              <li>
-                                <a href="#">Boots</a>
-                              </li>
-                              <li>
-                                <a href="#">Wide Fit</a>
-                              </li>
-                              <li>
-                                <a href="#">Slippers</a>
-                              </li>
-                            </ul>
-                          </div>
-                          <div class="col-5">
-                            <a href="category.html" class="mega-menu-title">Accessories</a>
-                            <ul class="mega-menu-list clearfix">
-                              <li>
-                                <a href="#">Bags &amp; Purses</a>
-                              </li>
-                              <li>
-                                <a href="#">Belts</a>
-                              </li>
-                              <li>
-                                <a href="#">Gloves</a>
-                              </li>
-                              <li>
-                                <a href="#">Jewellery</a>
-                              </li>
-                              <li>
-                                <a href="#">Sunglasses</a>
-                              </li>
-                              <li>
-                                <a href="#">Hair Accessories</a>
-                              </li>
-                            </ul>
-                          </div>
-                          <div class="col-5">
-                            <a href="category.html" class="mega-menu-title">Sports</a>
-                            <ul class="mega-menu-list clearfix">
-                              <li>
-                                <a href="#">Sport Tops &amp; Vests</a>
-                              </li>
-                              <li>
-                                <a href="#">Swimwear</a>
-                              </li>
-                              <li>
-                                <a href="#">Footwear</a>
-                              </li>
-                              <li>
-                                <a href="#">Sports Underwear</a>
-                              </li>
-                              <li>
-                                <a href="#">Bags</a>
-                              </li>
-                            </ul>
-                          </div>
-                          <div class="col-5">
-                            <a href="category.html" class="mega-menu-title">Maternity</a>
-                            <ul class="mega-menu-list clearfix">
-                              <li>
-                                <a href="#">Tops &amp; Skirts</a>
-                              </li>
-                              <li>
-                                <a href="#">Dresses</a>
-                              </li>
-                              <li>
-                                <a href="#">Trousers &amp; Shorts</a>
-                              </li>
-                              <li>
-                                <a href="#">Knitwear</a>
-                              </li>
-                              <li>
-                                <a href="#">Jackets &amp; Coats</a>
-                              </li>
-                            </ul>
-                          </div>
-                        </div>
-                      </li>
-                      <li>
-                        <a href="#">PAGES</a>
-                        <ul>
-                          <li>
-                            <a href="#">Headers</a>
-                            <ul>
-                              <li>
-                                <a href="header1.html">Header Version 1</a>
-                              </li>
-                              <li>
-                                <a href="header2.html">Header Version 2</a>
-                              </li>
-                              <li>
-                                <a href="header3.html">Header Version 3</a>
-                              </li>
-                              <li>
-                                <a href="header4.html">Header Version 4</a>
-                              </li>
-                              <li>
-                                <a href="header5.html">Header Version 5</a>
-                              </li>
-                              <li>
-                                <a href="header6.html">Header Version 6</a>
-                              </li>
-                              <li>
-                                <a href="header7.html">Header Version 7</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="#">Footers</a>
-                            <ul>
-                              <li>
-                                <a href="footer1.html">Footer Version 1</a>
-                              </li>
-                              <li>
-                                <a href="footer2.html">Footer Version 2</a>
-                              </li>
-                              <li>
-                                <a href="footer3.html">Footer Version 3</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="product.html">Product</a>
-                          </li>
-                          <li>
-                            <a href="cart.html">Cart</a>
-                          </li>
-                          <li>
-                            <a href="category.html">Category</a>
-                            <ul>
-                              <li>
-                                <a href="category-list.html">Category list</a>
-                              </li>
-                              <li>
-                                <a href="category.html">Category Banner 1</a>
-                              </li>
-                              <li>
-                                <a href="category-banner-2.html">Category Banner 2</a>
-                              </li>
-                              <li>
-                                <a href="category-banner-3.html">Category Banner 3</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="blog.html">Blog</a>
-                            <ul>
-                              <li>
-                                <a href="blog.html">Right Sidebar</a>
-                              </li>
-                              <li>
-                                <a href="blog-sidebar-left.html">Left Sidebar</a>
-                              </li>
-                              <li>
-                                <a href="blog-sidebar-both.html">Both Sidebar</a>
-                              </li>
-                              <li>
-                                <a href="single.html">Blog Post</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="checkout.html">Checkout</a>
-                          </li>
-                          <li>
-                            <a href="aboutus.html">About Us</a>
-                          </li>
-                          <li>
-                            <a href="register-account.html">Register Account</a>
-                          </li>
-                          <li>
-                            <a href="compare-products.html">Compare Products</a>
-                          </li>
-                          <li>
-                            <a href="login.html">Login</a>
-                          </li>
-                          <li>
-                            <a href="404.html">404 Page</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href="#">Portfolio</a>
-                        <ul>
-                          <li>
-                            <a href="#">Classic</a>
-                            <ul>
-                              <li>
-                                <a href="portfolio-2.html">Two Columns</a>
-                              </li>
-                              <li>
-                                <a href="portfolio-3.html">Three Columns</a>
-                              </li>
-                              <li>
-                                <a href="portfolio-4.html">Four Columns</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="#">Masonry</a>
-                            <ul>
-                              <li>
-                                <a href="portfolio-masonry-2.html">Two Columns</a>
-                              </li>
-                              <li>
-                                <a href="portfolio-masonry-3.html">Three Columns</a>
-                              </li>
-                              <li>
-                                <a href="portfolio-masonry-4.html">Four Columns</a>
-                              </li>
-                            </ul>
-                          </li>
-                          <li>
-                            <a href="#">Portfolio Posts</a>
-                            <ul>
-                              <li>
-                                <a href="single-portfolio.html">Image Post</a>
-                              </li>
-                              <li>
-                                <a href="single-portfolio-gallery.html">Gallery Post</a>
-                              </li>
-                              <li>
-                                <a href="single-portfolio-video.html">Video Post</a>
-                              </li>
-                            </ul>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href="#">Elements</a>
-                        <ul>
-                          <li>
-                            <a href="elements/tabs.html">Tabs</a>
-                          </li>
-                          <li>
-                            <a href="elements/titles.html">Titles</a>
-                          </li>
-                          <li>
-                            <a href="elements/typography.html">Typography</a>
-                          </li>
-                          <li>
-                            <a href="elements/collapses.html">collapses</a>
-                          </li>
-                          <li>
-                            <a href="elements/animations.html">animations</a>
-                          </li>
-                          <li>
-                            <a href="elements/grids.html">Grid System</a>
-                          </li>
-                          <li>
-                            <a href="elements/alerts.html">Alert Boxes</a>
-                          </li>
-                          <li>
-                            <a href="elements/buttons.html">Buttons</a>
-                          </li>
-                          <li>
-                            <a href="elements/medias.html">Media</a>
-                          </li>
-                          <li>
-                            <a href="elements/forms.html">Forms</a>
-                          </li>
-                          <li>
-                            <a href="elements/icons.html">Icons</a>
-                          </li>
-                          <li>
-                            <a href="elements/lists.html">Lists</a>
-                          </li>
-                          <li>
-                            <a href="elements/more.html">More</a>
-                          </li>
-                        </ul>
-                      </li>
-                      <li>
-                        <a href="contact.html">Contact Us</a>
-                      </li>
+                      <li> <a href="<?php echo e(url('/')); ?>">HOME</a> </li>
+                      <li> <a href="<?php echo e(url('shop')); ?>">shop</a> </li>
+                      <li> <a href="<?php echo e(url('categories')); ?>">categories</a> </li>
+                      <li> <a href="<?php echo e(url('about')); ?>">about</a> </li>
+                      <li> <a href="<?php echo e(url('contact')); ?>">Contact Us</a>  </li>
                     </ul>
                   </nav>
                   <div id="quick-access">
@@ -609,10 +271,10 @@ $bday = $isLoggedIn ? $user->bday : '';
                           </ul>
                           <div class="dropdown-cart-action">
                             <p>
-                              <a href="cart.html" class="btn btn-custom-2 btn-block">Cart</a>
+                              <a href="<?php echo e(url('cart')); ?>" class="btn btn-custom-2 btn-block">Cart</a>
                             </p>
                             <p>
-                              <a href="checkout.html" class="btn btn-custom btn-block">Checkout</a>
+                              <a href="<?php echo e(url('checkout')); ?>" class="btn btn-custom btn-block">Checkout</a>
                             </p>
                           </div>
                         </div>
@@ -1063,6 +725,17 @@ $bday = $isLoggedIn ? $user->bday : '';
  <!-- JS here -->
  <script src="js/jquery.js"></script>
    <script src="js/bootstrap.min.js"></script> 
+   <script src="js/smoothscroll.js"></script>
+    <script src="js/jquery.debouncedresize.js"></script>
+    <script src="js/retina.min.js"></script>
+    <script src="js/jquery.placeholder.js"></script>
+    <script src="js/jquery.hoverIntent.min.js"></script>
+    <script src="js/twitter/jquery.tweet.min.js"></script>
+    <script src="js/jquery.flexslider-min.js"></script>
+    <script src="js/owl.carousel.min.js"></script>
+    <script src="js/jflickrfeed.min.js"></script>
+    <script src="js/jquery.prettyPhoto.js"></script>
+    <script src="js/colpick.js"></script>
    <script src="js/main.js"></script>
    <script src="js/helpers.js?ver=<?php echo e(rand(999, 9999999)); ?>"></script>
    <script src="lib/sweet-alert/sweetalert2.js"></script>
